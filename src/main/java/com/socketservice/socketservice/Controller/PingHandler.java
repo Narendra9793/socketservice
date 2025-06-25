592b73fec89758d42166420cb1972b978e151b76
Narendra Pratap Singh <72899015+Narendra9793@users.noreply.github.com>
2025-06-25 10:24:34 +0530

    fixed

--- src/main/java/com/socketservice/socketservice/Controller/PingHandler.java
+++ src/main/java/com/socketservice/socketservice/Controller/PingHandler.java
@@ -1,7 +1,11 @@
 package com.socketservice.socketservice.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Restcontroller
+@RestController
 @RequestMapping("/api/")
 public class PingHandler{
 
